a8acbf4bd544088508103fd93f5f782692a8f4f2
gusxdev <agussetiawan.me02@gmail.com>
2024-01-24 22:35:12 +0700

    add tailwind

---
+++ src/data.ts
@@ -0,0 +1,9 @@
+export const profile = {
+    avatar: `/profile.png`,
+    name: `Agus Setiawan`,
+    bio: `Just a guy that likes to code..`,
+  };
+
+export const year = {
+  text: new Date().getFullYear()
+}
